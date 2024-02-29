a8420ec2d75dc649b241d4fc1f23480c376c9d72
sadiyacodes <99393549+sadiyacodes@users.noreply.github.com>
2024-02-29 22:57:08 +0530

    added function to persist data after refresh

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container")
+const listContainer = document.getElementById("list-container");
 
 function addTask() {
     if(inputBox.value === ''){
@@ -14,16 +14,28 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
-listContainer.addEventListener("click", function(e))
+listContainer.addEventListener("click", function(e)
 {
     if(e.target.tagName === "LI")
     {
        e.target.classList.toggle("checked"); 
+       saveData();
     }
     else if(e.target.tagName==="SPAN")
     {
         e.target.parentElement.remove();
+        saveData();
     }
+}, false);
+function saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML);
 }
+function showTask()
+{
+listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
